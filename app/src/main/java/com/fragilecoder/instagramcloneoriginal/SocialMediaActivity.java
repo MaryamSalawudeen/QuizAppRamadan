17fc83f3a6eb63ee2b0bae8a5fa7704f1e4ce9d0
thatfragilemuslimah <shecodeswell@gmail.com>
2020-06-02 20:12:18 +0100

    Tab Layout SetUp

--- app/src/main/java/com/fragilecoder/instagramcloneoriginal/SocialMediaActivity.java
+++ app/src/main/java/com/fragilecoder/instagramcloneoriginal/SocialMediaActivity.java
@@ -3,12 +3,29 @@ package com.fragilecoder.instagramcloneoriginal;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class SocialMediaActivity extends AppCompatActivity {
 
+    private androidx.appcompat.widget.Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabAdapter tabAdapter;
+    private TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_media);
+        setTitle("Social Media App");
+        toolbar = findViewById(R.id.mytoolbar);
+        setSupportActionBar(toolbar);
+
+        viewPager = findViewById(R.id.viewPager);
+        tabAdapter = new TabAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabAdapter);
+        tabLayout = findViewById(R.id.tablayout);
+        tabLayout.setupWithViewPager(viewPager , true);
+
     }
 }
