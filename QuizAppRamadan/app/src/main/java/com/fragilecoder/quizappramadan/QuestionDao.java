df97d05bfdbf1ffe060b5cf10be5088aa8137946
thatfragilemuslimah <shecodeswell@gmail.com>
2020-06-14 17:10:04 +0100

    Creating Database Class

--- QuizAppRamadan/app/src/main/java/com/fragilecoder/quizappramadan/QuestionDao.java
+++ QuizAppRamadan/app/src/main/java/com/fragilecoder/quizappramadan/QuestionDao.java
@@ -2,6 +2,7 @@ package com.fragilecoder.quizappramadan;
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Insert;
 import androidx.room.Query;
 
 import java.util.List;
@@ -11,5 +12,7 @@ import java.util.List;
 public interface QuestionDao {
     @Query("SELECT * from  questions_table")
     LiveData<List<Questions>> getAllQuestions();
+    @Insert
+    void insert (Questions questions);
 
 }
---
+++ QuizAppRamadan/app/src/main/java/com/fragilecoder/quizappramadan/QuestionRoomDatabase.java
@@ -0,0 +1,67 @@
+package com.fragilecoder.quizappramadan;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+@Database(entities = {Questions.class},version = 1)
+public abstract class QuestionRoomDatabase extends RoomDatabase {
+    private static QuestionRoomDatabase INSTANCE;
+
+    public abstract QuestionDao questionDao();
+    public static synchronized QuestionRoomDatabase getINSTANCE(final Context context) {
+        if (INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+            QuestionRoomDatabase.class, "questions_database")
+                    .fallbackToDestructiveMigration()
+                    .addCallback(ROOMDBCallBack)
+                    .build();
+        }
+        return INSTANCE;
+    }
+    private static RoomDatabase.Callback ROOMDBCallBack= new RoomDatabase.Callback() {
+
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+
+            new PopulateDbAsyncTask(INSTANCE).execute();
+
+
+        }
+    };
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void,Void,Void> {
+        private QuestionDao questionDao;
+
+        private PopulateDbAsyncTask(QuestionRoomDatabase db) {
+
+            questionDao = db.questionDao();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            questionDao.insert(new Questions("In what chapter of the quran did Allah teach us the etiquettes of entering into somebody else’s house?" ,
+                    "Suratul Ankabut","Suratul Mujadilah","Suratun Noor","Suratul Maryam",3));
+            questionDao.insert(new Questions("What are the names of the idols worshipped in Makkah (before the advent of Islam)  mentioned \n" +
+                    "    in the Quran? " ,
+                    "Laat, Uzzah, Manaat","Laat , Ghurapath, Manaat","Ghurapath, Lomu, ffagu","Ffagu,Laat,Uzzah",1));
+            questionDao.insert(new Questions("Pick 5 prophets sent to Banu Israee." ,
+                    "Dawood,Isa,Musa,Shammeel,Hizqeel","Adam,Nuh,Yushau,Shammeel,Dhul Khifl","Muhammad,Musa,Adam,Yusuf,Isa","Isa, Musa, Yusuf, Yushau, Shammeel ",4));
+            questionDao.insert(new Questions(" “La ilaha illalah lahu l mulk wa lahu l hamdu yuhyee wa yumeet wa huwa hayyu la yamut  \n" +
+                    "    biyadihi l khayr wa huwa ala kulli shaeyin Qadeer” is a prayer made when?" ,
+                    "when entering the toilet","when wearing new clothe","when entering the market","when waking up",3));
+
+
+
+            return null;
+        }
+    }
+
+}
